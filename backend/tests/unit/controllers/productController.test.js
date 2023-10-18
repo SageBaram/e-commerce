b677727548197ed500af9796af094f1c40d70525
Sage Baram <sagebaram.career@gamil.com>
2023-10-18 22:34:17 +0300

    test: add more descriptive tests, handle errors explicitly

--- backend/tests/unit/controllers/productController.test.js
+++ backend/tests/unit/controllers/productController.test.js
@@ -5,6 +5,7 @@ const Product = require("@models/productModel");
 
 app.use("/api/products", require("@routes/productRoutes"));
 
+// Product mockup
 const mockProduct = {
 	_id: "Sample Id",
 	name: "Sample Product",
@@ -16,164 +17,174 @@ const mockProduct = {
 	countInStock: 10,
 };
 
+// Commonly used variables in various tests
+const productId = "Sample Id";
+const updateData = { name: "Updated Name" };
+
 // Product Controller Tests
 describe("Product Controller", () => {
+	// Function mockups
 	let saveMock;
 	let findMock;
 	let findByIdMock;
 	let findOneAndDeleteMock;
+
 	// Reset all mocks before each test
 	beforeEach(() => {
 		saveMock = jest.spyOn(Product.prototype, "save");
 		findMock = jest.spyOn(Product, "find");
 		findByIdMock = jest.spyOn(Product, "findById");
 		findOneAndDeleteMock = jest.spyOn(Product, "findOneAndDelete");
+	});
 
-		// Reset the mock implementations and clear any previous mock calls
-		saveMock.mockReset();
-		findMock.mockReset();
-		findByIdMock.mockReset();
-		findOneAndDeleteMock.mockReset();
+	afterEach(() => {
+		jest.clearAllMocks();
 	});
 
 	// Test for createProduct
-	it("should create a new product", async () => {
-		// Mock the behavior of the Product.save method
-		saveMock.mockResolvedValue(mockProduct);
+	describe("Product CRUD", () => {
+		describe("Create Product", () => {
+			it("should successfully create a new product and return it in the response body with a 201 status code", async () => {
+				// Mock the behavior of the Product.save method
+				const { _id, ...newProduct } = mockProduct;
 
-		const newProduct = {
-			name: "Sample Product",
-			brand: "Sample Brand",
-			category: "Sample Category",
-			description: "Sample Description",
-			image: "Image Sample",
-			price: 100,
-			countInStock: 10,
-		};
+				saveMock.mockResolvedValue(newProduct);
+				const res = await request(app).post("/api/products/").send(newProduct);
 
-		const res = await request(app).post("/api/products/").send(newProduct);
+				expect(res.statusCode).toEqual(201);
+				expect(res.body).toHaveProperty("product");
+				expect(saveMock).toHaveBeenCalled();
+			});
 
-		expect(res.statusCode).toEqual(201);
-		expect(res.body).toHaveProperty("product");
+			it("should return a 500 status code and an error message when an exception is thrown during product creation", async () => {
+				saveMock.mockRejectedValue(new Error("Error during product creation"));
 
-		expect(saveMock).toHaveBeenCalled();
-	});
+				const res = await request(app).post("/api/products/").send();
 
-	// Test for readAllProducts
-	it("should fetch all products", async () => {
-		// Mock the behaviour of the Product.find method
-		findMock.mockResolvedValue([mockProduct]);
+				expect(res.statusCode).toEqual(500);
 
-		const res = await request(app).get("/api/products/");
-		expect(res.statusCode).toEqual(200);
-		expect(res.body).toHaveProperty("products");
-		expect(Array.isArray(res.body.products)).toBeTruthy();
-		expect(findMock).toHaveBeenCalled();
-	});
+				expect(saveMock).toHaveBeenCalled();
+				expect(findMock).not.toHaveBeenCalled();
+			});
+		});
+		describe("Fetch all products", () => {
+			// Test for readAllProducts
+			it("should successfully fetch all products, return them in the response body, and have a 200 status code", async () => {
+				// Mock the behaviour of the Product.find method
+				findMock.mockResolvedValue([mockProduct]);
 
-	// Test for readProduct
-	it("should fetch a single product by ID", async () => {
-		// Mock the behaviour of the Product.findById method
-		findByIdMock.mockResolvedValue(new Product(mockProduct));
+				const res = await request(app).get("/api/products/");
+				expect(res.statusCode).toEqual(200);
+				expect(res.body).toHaveProperty("products");
+				expect(Array.isArray(res.body.products)).toBeTruthy();
+				expect(findMock).toHaveBeenCalled();
+			});
 
-		const productId = "Sample Id";
-		const res = await request(app).get(`/api/products/${productId}`);
+			it("should return a 500 status code and an error message when an exception is thrown during fetching all products", async () => {
+				// Mock the behaviour of the Product.find method
+				findMock.mockRejectedValue(
+					new Error("Error during fetching all products"),
+				);
 
-		if (res.statusCode === 200) {
-			expect(res.body).toHaveProperty("product");
-			expect(typeof res.body.product).toBe("object");
-			expect(findByIdMock).toHaveBeenCalled();
-		} else {
-			expect(res.statusCode).toEqual(404);
-			expect(res.body).toHaveProperty("message", "Product not found");
-		}
-	});
+				const res = await request(app).get("/api/products/");
 
-	// Test for successful update
-	it("should successfully update a product by ID", async () => {
-		const productId = "Sample Id";
-		const updateData = { name: "Updated Name" };
+				expect(res.statusCode).toEqual(500);
+				expect(Array.isArray(res.body.products)).toBeFalsy();
+				expect(findMock).toHaveBeenCalled();
+				expect(saveMock).not.toHaveBeenCalled();
+			});
+		});
+		describe("Fetch a product", () => {
+			// Test for readProduct
+			it("should successfully fetch a single product by its ID and return it in the response body with a 200 status code", async () => {
+				findByIdMock.mockResolvedValue(new Product(mockProduct));
 
-		findByIdMock.mockResolvedValue(new Product(mockProduct));
-		saveMock.mockResolvedValue(mockProduct);
+				const res = await request(app).get(`/api/products/${productId}`);
 
-		const res = await request(app)
-			.patch(`/api/products/${productId}`)
-			.send(updateData);
+				expect(res.statusCode).toEqual(200);
+				expect(res.body).toHaveProperty("product");
+				expect(typeof res.body.product).toBe("object");
+				expect(findByIdMock).toHaveBeenCalled();
+			});
+			it("should return a 404 status code and an error message when the product ID is not found in the database", async () => {
+				findByIdMock.mockResolvedValue(null);
 
-		expect(res.statusCode).toEqual(200);
-		expect(res.body).toHaveProperty("product");
-		expect(res.body.product.name).toEqual("Updated Name");
-		expect(findByIdMock).toHaveBeenCalled();
-		expect(saveMock).toHaveBeenCalled();
-	});
+				const res = await request(app).get(`/api/products/${productId}`);
 
-	it("should delete a product by ID", async () => {
-		const productId = "Sample Id";
+				expect(res.statusCode).toEqual(404);
+			});
+			it("should return a 500 status code and an error message when an exception is thrown during fetching a single product", async () => {
+				findByIdMock.mockRejectedValue(
+					new Error("Error during fetching a single product"),
+				);
 
-		findOneAndDeleteMock.mockResolvedValue(mockProduct);
-		const res = await request(app).delete(`/api/products/${productId}`);
+				const res = await request(app).get(`/api/products/${productId}`);
 
-		if (res.statusCode === 201) {
-			expect(res.body).toHaveProperty(
-				"message",
-				`${productId} Deleted from database!`,
-			);
-		} else {
-			expect(res.statusCode).toEqual(500);
-			expect(res.body).toHaveProperty("message", "Product not found");
-			expect(findOneAndDeleteMock).toHaveBeenCalled();
-		}
-	});
+				expect(res.statusCode).toEqual(500);
+				expect(findByIdMock).toHaveBeenCalled();
+			});
+		});
+		describe("Update a product", () => {
+			// Successful update
+			it("should successfully update a product by its ID, return the updated product in the response body, and have a 200 status code", async () => {
+				findByIdMock.mockResolvedValue(new Product(mockProduct));
+				saveMock.mockResolvedValue(mockProduct);
 
-	// Test for product not found
-	it("should return 404 if product is not found", async () => {
-		const productId = "Sample Id";
+				const res = await request(app)
+					.patch(`/api/products/${productId}`)
+					.send(updateData);
 
-		findByIdMock.mockResolvedValue(null);
+				expect(res.statusCode).toEqual(200);
+				expect(res.body).toHaveProperty("product");
+				expect(res.body.product.name).toEqual("Updated Name");
+				expect(findByIdMock).toHaveBeenCalled();
+				expect(saveMock).toHaveBeenCalled();
+			});
 
-		const res = await request(app).patch(`/api/products/${productId}`);
+			it("should return a 404 status code and an error message when the product ID is not found in the database for updating", async () => {
+				findByIdMock.mockResolvedValue(null);
 
-		expect(res.statusCode).toEqual(404);
-		expect(res.body).toHaveProperty("message", "Product not found");
-		expect(findByIdMock).toHaveBeenCalled();
-	});
+				const res = await request(app)
+					.patch(`/api/products/${productId}`)
+					.send(updateData);
 
-	// Test for database error during findById
-	it("should return 500 if an error occurs during findById", async () => {
-		const productId = "Sample Id";
-		const updateData = { name: "Updated Name" };
+				expect(res.statusCode).toEqual(404);
+				expect(findByIdMock).toHaveBeenCalled();
+				expect(saveMock).not.toHaveBeenCalled();
+			});
 
-		Product.findById = jest.fn(() =>
-			Promise.reject(new Error("Test error during findById")),
-		);
+			it("should return a 500 status code and an error message when an exception is thrown during the product update process", async () => {
+				findByIdMock.mockResolvedValue(new Product(mockProduct));
+				saveMock.mockRejectedValue(new Error("Error during product update"));
 
-		const res = await request(app)
-			.patch(`/api/products/${productId}`)
-			.send(updateData);
+				const res = await request(app)
+					.patch(`/api/products/${productId}`)
+					.send(updateData);
 
-		expect(res.statusCode).toEqual(500);
-		expect(res.body).toHaveProperty("error");
-		expect(Product.findById).toHaveBeenCalled();
-	});
+				expect(res.statusCode).toEqual(500);
+				expect(findByIdMock).toHaveBeenCalled();
+				expect(saveMock).toHaveBeenCalled();
+			});
+		});
+		describe("Delete product", () => {
+			it("should successfully delete a product by its ID and return a confirmation message with a 200 status code", async () => {
+				findOneAndDeleteMock.mockResolvedValue(mockProduct);
+				const res = await request(app).delete(`/api/products/${productId}`);
+				expect(res.statusCode).toEqual(200);
+				expect(res.body).toHaveProperty(
+					"message",
+					`${productId} Deleted from database!`,
+				);
+				expect(findOneAndDeleteMock).toHaveBeenCalled();
+			});
 
-	// Test for server error during update
-	it("should return 500 if an error occurs during update", async () => {
-		const productId = "Sample Id";
-		const updateData = { name: "Updated Name" };
+			it("should return a 404 status code and an error message when the product ID is not found in the database for deletion", async () => {
+				findOneAndDeleteMock.mockRejectedValue(null);
+				const res = await request(app).delete(`/api/products/${productId}`);
 
-		findByIdMock.mockResolvedValue(new Product(mockProduct));
-		Product.prototype.save = jest.fn(() =>
-			Promise.reject(new Error("Test save error")),
-		);
-
-		const res = await request(app)
-			.patch(`/api/products/${productId}`)
-			.send(updateData);
-
-		expect(res.statusCode).toEqual(500);
-		expect(res.body).toHaveProperty("error");
-		expect(findByIdMock).toHaveBeenCalled();
-		expect(Product.prototype.save).toHaveBeenCalled();
+				expect(res.statusCode).toEqual(404);
+				expect(findOneAndDeleteMock).toHaveBeenCalled();
+			});
+		});
 	});
 });
